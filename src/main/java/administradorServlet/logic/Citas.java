74f04446d94cb98a4320eebfeaa463eb22aa3cd9
Gabs2308 <74335369+Gabs2308@users.noreply.github.com>
2022-04-04 20:15:02 -0600

    Merge origin/master
    Conflicts:
    	src/main/java/administradorServlet/logic/Pacie

---
+++ src/main/java/administradorServlet/logic/Citas.java
@@ -0,0 +1,15 @@
+package administradorServlet.logic;
+
+public class Citas {
+    private double id;
+    private String medico;
+
+    public Citas(double id, String medico) {
+        this.id = id;
+        this.medico = medico;
+    }
+
+    public Citas() {
+
+    }
+}
